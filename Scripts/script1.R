0cb63a60b08178609d52308c7d53c1249ac503fd
Charlotte Head <72808053+charlierose22@users.noreply.github.com>
2022-11-29 23:20:56 +0000

    Biosolid Only Graph

--- Scripts/script1.R
+++ Scripts/script1.R
@@ -3,23 +3,38 @@ setwd("~/GitHub/mass-spec-2021")
 
 # load (and/or install) packages
 library(tidyverse)
+library(readxl)
 library(readr)
+library(dplyr)
 
 # import peak area df
-PeakAreas <- read_excel("Raw Data/NaburnPeakAreas.xlsx")
+NaburnPeakAreas <- read_excel("Raw Data/NaburnPeakAreas.xlsx", 
+                              +     n_max = 25)
 
 # import cv df
 NaburnCVs <- read_excel("Raw Data/NaburnCVs.xlsx")
 
+# drop columns to keep only biosolids
+Biosolid <- select(NaburnPeakAreas, -c("AD Feed Pump":"AD Tank 4"))
+
 # pivot both longer 
 PeakAreaLong <- pivot_longer(PeakAreas,
-                       !Name,
-                       names_to = "TreatmentStage", 
-                       values_to = "PeakAreas")
-CVLong <- pivot_longer(NaburnCVs,
                              !Name,
                              names_to = "TreatmentStage", 
-                             values_to = "CV")
+                             values_to = "PeakAreas")
+CVLong <- pivot_longer(NaburnCVs,
+                       !Name,
+                       names_to = "TreatmentStage", 
+                       values_to = "CV")
+BiosolidLong <- pivot_longer(Biosolid,
+                             !Name,
+                             names_to = "TreatmentStage",
+                             values_to = "PeakAreas")
+# rename some columns
+RenamedBiosolid <- mutate(BiosolidLong, TreatmentStage = case_when(
+  str_detect(TreatmentStage, "Fresh") ~ "1 Fresh Biosolid",
+  str_detect(TreatmentStage, "Dried") ~ "2 Aged Biosolid",
+  str_detect(TreatmentStage, "Control") ~ "3 Control"))
 
 Rearranged_PeakAreaLong <- mutate(PeakAreaLong, TreatmentStage = case_when(
   str_detect(TreatmentStage, "Feed Pump") ~ "A",
@@ -32,14 +47,26 @@ Rearranged_PeakAreaLong <- mutate(PeakAreaLong, TreatmentStage = case_when(
   str_detect(TreatmentStage, "Output") ~ "H",
   str_detect(TreatmentStage, "Control") ~ "I"))
 
-NoNA <- drop_na(Rearranged_PeakAreaLong)
+NoNA <- drop_na(RenamedBiosolid)
 
 # heatmaps
-mycol <- c("navy", "blue", "cyan", "lightcyan", "yellow", "red", "red4")
+mycol <- c("navy","blue", "cyan", "light cyan", "yellow", "red", "red4")
 ggplot(NoNA, aes(y = Name, x = TreatmentStage, fill = PeakAreas)) +
   geom_tile(aes(fill = PeakAreas)) +
   scale_fill_gradientn(colours = mycol, trans = "log") +
   labs(x = "Wastewater Treatment Stage", 
-       y = "Compound", 
+       y = "Antimicrobial Compound", 
        fill = "Mean Peak Area") +
   theme_bw()
+
+ggplot(NoNA, aes(y = TreatmentStage, x = Name, fill = PeakAreas)) +
+  geom_tile(aes(fill = PeakAreas)) +
+  scale_fill_gradientn(colours = mycol, trans = "log") +
+  labs(x = "Antimicrobial Compound", 
+       y = "Wastewater Treatment Stage", 
+       fill = "Mean Peak Area") +
+  theme(axis.text.x = element_text(angle = 90)) +
+  theme_light()
+
+
+
